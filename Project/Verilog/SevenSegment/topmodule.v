a331a9ce733394aeabf60607cf577aba51ddbb54
Aditya Mehta <aditya.mehta@iitgn.ac.in>
2024-11-06 04:46:17 +0530

    Nov 6 - Added topmodule to integrate IF with clock

--- Project/Verilog/SevenSegment/topmodule.v
+++ Project/Verilog/SevenSegment/topmodule.v
@@ -1,22 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 26.10.2024 00:06:44
-// Design Name: 
-// Module Name: topmodule
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 
 module topmodule (
     input clk,
